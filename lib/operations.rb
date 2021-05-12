70e3497867f5655120960b0b30c9f4cf64c8cb47
luis <foreverluiscastro@gmail.com>
2021-05-12 02:11:23 -0400

    commit tree 2f4c6750523e0a3b076a8769115a752ad538595a

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,11 @@
 def unsafe?(speed)
-
+    speed ? speed < 40 || speed > 60 : speed > 40 && speed < 60
 end
 
 
 
 def not_safe?(speed)
-	
+	speed ? speed > 60 || speed < 40 : speed > 40 && speed < 60
 end
 	
 
